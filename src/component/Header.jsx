821376d48c52c8c57cb99bb62f0b13e19569cde1
Roshu-git <roshaniadlak15@gmail.com>
2026-08-07 22:22:15 +0530

    header section is created

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSearchTerm } from '../redux/cartSlice';
 
-function Header() {
+export default function Header() {
+  const count = useSelector(state =>
+    state.cart.items.reduce((a, c) => a + c.quantity, 0)
+  );
+
+  const dispatch = useDispatch();
+
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <Link to="/">ShoppyGlobe</Link>
 
-export default Header
+      <input
+        placeholder="Search products..."
+        onChange={(e) => dispatch(setSearchTerm(e.target.value))}
+      />
+
+      <Link to="/cart">🛒 {count}</Link>
+    </header>
+  );
+}
